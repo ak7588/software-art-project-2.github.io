ea60d80dfd3c5c8166be0f6b145219df256c27d0
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-02-14 19:33:01 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// tutorial credits:
+// https://tobiasahlin.com/moving-letters/#3
+
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  const btn = document.querySelector(".btn");
+  btn.addEventListener('click', () => {
+    alert("Connecting to API...Success!");
+    console.log(btn.classList);
+  });
